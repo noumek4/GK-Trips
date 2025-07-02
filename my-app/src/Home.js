07f78e0621f8e54656dd9f6591c05c80c83f7b79
noumek4 <noumek.aaron@giortech.com>
2025-07-02 19:43:05 +0100

    new updtes for home

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,1 +1,22 @@
 import React from 'react'
+import {FaSearch} from 'react-icons/fa'
+
+function Home() {
+    return (
+        <>
+        <nav className='navbar'>
+            <FaSearch className='search-icon' 
+            size={20}/>
+            <input type='text' placeholder='Search for trips, Hotels' className='search-input' />
+            <div className='nav-links'>
+                <ul>
+                    <li><a href='./Auth.js'>Create an Account</a></li>
+                    <li><a href='./Signin.js'>Sign In</a></li>
+                </ul>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Home
