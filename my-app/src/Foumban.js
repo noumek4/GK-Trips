afcf5775cfc7ce80d9ead1467f96d519c2f93e5d
noumek4 <noumek.aaron@giortech.com>
2025-07-03 15:00:06 +0100

    updated

---
+++ my-app/src/Foumban.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Foumban() {
+    return (
+        <div className='header'>
+            <div className='trip-header'>Trip Info</div>
+
+            <div className='trip-info-box'>
+                <img src="/assets/bg3.jpg" alt="Foumban Museum" className="trip-img" />
+                <div className='trip-description'>
+                    <h2>The Foumban Museum</h2>
+                    <p>Known for it's incredible design the waza park is known for
+                        it's incredible design and the variety of animals it houses and 
+                        it's traditional artefact
+                    </p>
+                </div>
+            </div>
+           
+        </div>
+
+        
+        
+    )
+}
+
+export default Foumban;
