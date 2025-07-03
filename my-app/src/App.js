ce1ada40d8276441534d68994ebf47a67cd63ea8
noumek4 <noumek.aaron@giortech.com>
2025-07-03 17:07:09 +0100

    react dom install

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,27 @@
-
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import './App.css';
 import Auth from './Auth';
+import Home from './Home';
+import Foumban from './Foumban';
 import './Auth.css'
+
 
 function App() {
   return (
-    <><Auth /></>
+    <Router>
+      <nav style={{ padding:20, background:'#eee'}}>
+        <Link to="/Auth" style={{marginRight: 10}}>Create an account</Link>
+        <Link to="/Foumban" style={{marginRight: 10}}>Foumban</Link>
+        <Link to="/Home" style={{marginRight: 10}}>Home</Link>
+      </nav>
+
+      <Routes>
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/foumban' element={<Foumban />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </Router>
+   
     
   );
 }
