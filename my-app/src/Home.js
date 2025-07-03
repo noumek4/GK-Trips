28d49a04c989051b00cbe57243bdb9c2d4b69408
noumek4 <noumek.aaron@giortech.com>
2025-07-03 10:03:07 +0100

    add css

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa'
+import './Home.css'; 
+
+const cardData = [
+    { title: 'The Waza Park', Image: '/assets/bg3.jpg'},
+    { title: 'The Foumban Museum', Image: '/assets/bg3.jpg'},
+    { title: 'The Waza Park', Image: '/assets/bg3.jpg'},
+    { title: 'The Mount Febe Hotel', Image: '/assets/bg3.jpg'},
+    { title: 'Douala Rabingha Hotel', Image: '/assets/bg3.jpg'},
+    { title: 'Jully Kribi Residence', Image: '/assets/bg3.jpg'}
+]
 
 function Home() {
     return (
@@ -15,6 +25,16 @@ function Home() {
                 </ul>
             </div>
         </nav>
+
+        <div className='card-container'>
+            {cardData.map((card, index) => (
+                <div className='card' key={index}>
+                    <img src={card.Image} alt={card.title} />
+                    <h3>{card.title}</h3>
+                    <button>View</button>
+                </div>
+            ))}
+        </div>
         </>
     )
 }
