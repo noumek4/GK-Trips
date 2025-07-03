07303214fe697747d2b2f75e67c4e7bf863a1b5e
noumek4 <noumek.aaron@giortech.com>
2025-07-03 10:24:13 +0100

    updated

---
+++ my-app/src/Signin.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useState } from 'react';
+import './Auth.css'
+import bg1 from './assets/bg1.jpg';
+import bg2 from './assets/bg2.jpg';
+import bg3 from './assets/bg3.jpg';
+import bg4 from './assets/bg4.jpg';
+
+const backgrounds = [bg1, bg2, bg3, bg4];
+
+function Auth() {
+const [bgIndex, setBgIndex] = useState(0);
+const [formData, setFormData] = useState({
+    name: '',
+    password: '',
+});
+
+const cycleBackground = () => {
+    setBgIndex((bgIndex + 1) % backgrounds.length);
+};
+
+const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+        ...prev,
+        [name]: value
+    }));
+};
+
+return (
+    <div className='auth-container' style={{ backgroundImage: `url(${backgrounds[bgIndex]})` }} onClick={cycleBackground}>
+        <div className='overlay'>
+            <div className='form-box'>
+                <h2>GK Trips Sign In</h2>
+                <div className='progressbar' style={{width: `60%`, backgroundColor:'white'}}></div>
+                <input
+                    type='text'
+                    name='name'
+                    placeholder='Enter Your Name'
+                    value={formData.name}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type='password'
+                    name='password'
+                    placeholder='Choose Your Password'
+                    value={formData.password}
+                    onChange={handleInputChange}
+                />
+
+                <div className='button-group'>
+                    <button onClick={() => alert('Submitted!')}>Sign In</button>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+}
+
+export default Auth
