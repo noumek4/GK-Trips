aaa6f07bdac5a46708e6d23c520580110be811b0
noumek4 <noumek.aaron@giortech.com>
2025-07-03 18:11:28 +0100

    ajustments  made for the cards

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -8,7 +8,11 @@ const cardData = [
     { title: 'The Waza Park', Image: '/assets/bg3.jpg'},
     { title: 'The Mount Febe Hotel', Image: '/assets/bg3.jpg'},
     { title: 'Douala Rabingha Hotel', Image: '/assets/bg3.jpg'},
-    { title: 'Jully Kribi Residence', Image: '/assets/bg3.jpg'}
+    { title: 'Jully Kribi Residence', Image: '/assets/bg3.jpg'},
+    { title: 'The Waza Park', Image: '/assets/bg3.jpg'},
+    { title: 'The Waza Park', Image: '/assets/bg3.jpg'},
+    { title: 'The Waza Park', Image: '/assets/bg3.jpg'},
+    { title: 'The Waza Park', Image: '/assets/bg3.jpg'},
 ]
 
 function Home() {
@@ -16,7 +20,7 @@ function Home() {
         <>
         <nav className='navbar'>
             <FaSearch className='search-icon' 
-            size={20}/>
+            size={20} color='white'/>
             <input type='text' placeholder='Search for trips, Hotels' className='search-input' />
             <div className='nav-links'>
                 <ul>
