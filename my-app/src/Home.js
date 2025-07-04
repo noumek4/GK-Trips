c5a63cd04cd8f11f2c450d007a2676b2514f5f25
noumek4 <noumek.aaron@giortech.com>
2025-07-04 04:49:19 +0100

    changes

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -2,16 +2,16 @@ import React from 'react'
 import './Home.css'; 
 
 const cardData = [
-    { title: 'The Waza Park', Image: '/assets/bg3.jpg'},
+    { title: 'The Waza Park', Image: './assets/bg1.jpg'},
     { title: 'The Foumban Museum', Image: '/assets/bg3.jpg'},
-    { title: 'The Waza Park', Image: '/assets/bg3.jpg'},
-    { title: 'The Mount Febe Hotel', Image: '/assets/bg3.jpg'},
-    { title: 'Douala Rabingha Hotel', Image: '/assets/bg3.jpg'},
+    { title: 'limbe Botanical Garden', Image: '/assets/bg3.jpg'},
+    { title: 'The Ekom Nkam Waterfalls', Image: '/assets/bg3.jpg'},
+    { title: 'Down Beach', Image: '/assets/bg3.jpg'},
     { title: 'Jully Kribi Residence', Image: '/assets/bg3.jpg'},
-    { title: 'The Waza Park', Image: '/assets/bg3.jpg'},
-    { title: 'The Waza Park', Image: '/assets/bg3.jpg'},
-    { title: 'The Waza Park', Image: '/assets/bg3.jpg'},
-    { title: 'The Waza Park', Image: '/assets/bg3.jpg'},
+    { title: 'The Mount Febe Hotel', Image: '/assets/bg3.jpg'},
+    { title: 'The Douala Rabingha Hotel', Image: '/assets/bg3.jpg'},
+    { title: 'The Adrien Beach Hotel', Image: '/assets/bg3.jpg'},
+    { title: 'The Residence Amar', Image: '/assets/bg3.jpg'},
 ]
 
 function Home() {
