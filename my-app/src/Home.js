14d0c34262c8b90d9292a312c7a6afdc3aa54179
noumek4 <noumek.aaron@giortech.com>
2025-07-04 13:41:55 +0100

    changement

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Home.css'; 
-import './bg1.jpg'
+import bg1 from './bg1.jpg';
+import bg2 from './bg2.jpg';
+import bg3 from './bg3.jpg';
+import bg4 from './bg4.jpg';
+import bg5 from './bg5.jpg';
+import bg6 from './bg6.jpg';
 
 const cardData = [
-    { title: 'The Waza Park', Image: './bg1.jpg', type:"trip"},
-    { title: 'The Foumban Museum', Image: './bg2.jpg', type:"trip"},
-    { title: 'limbe Botanical Garden', Image: './bg3.jpg', type:"trip"},
-    { title: 'The Ekom Nkam Waterfalls', Image: './bg4.jpg', type:"trip"},
-    { title: 'Down Beach', Image: './bg5.jpg', type:"trip"},
-    { title: 'Jully Kribi Residence', Image: './bg6.jpg', type:"hotel"},
-    { title: 'The Mount Febe Hotel', Image: './bg3.jpg', type:"hotel"},
-    { title: 'The Douala Rabingha Hotel', Image: './bg4.jpg', type:"hotel"},
-    { title: 'The Adrien Beach Hotel', Image: './bg5.jpg', type:"hotel"},
-    { title: 'The Residence Amar', Image: './bg6.jpg', type:"hotel"}, 
+    { title: 'The Waza Park', Image: bg1, type:"trip"},
+    { title: 'The Foumban Museum', Image: bg2, type:"trip"},
+    { title: 'limbe Botanical Garden', Image: bg3, type:"trip"},
+    { title: 'The Ekom Nkam Waterfalls', Image: bg4, type:"trip"},
+    { title: 'Down Beach', Image: bg5, type:"trip"},
+    { title: 'Jully Kribi Residence', Image: bg6, type:"hotel"},
+    { title: 'The Mount Febe Hotel', Image: bg3, type:"hotel"},
+    { title: 'The Douala Rabingha Hotel', Image: bg4, type:"hotel"},
+    { title: 'The Adrien Beach Hotel', Image: bg5, type:"hotel"},
+    { title: 'The Residence Amar', Image: bg6, type:"hotel"}, 
 ]
 
 function Home() {
