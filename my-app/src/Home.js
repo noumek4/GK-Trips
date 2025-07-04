351a82cc392affac06661bc5ac5085240e943aec
noumek4 <noumek.aaron@giortech.com>
2025-07-04 11:24:15 +0100

    changes

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -3,16 +3,16 @@ import './Home.css';
 import './bg1.jpg'
 
 const cardData = [
-    { title: 'The Waza Park', Image: './bg1.jpg'},
-    { title: 'The Foumban Museum', Image: './bg2.jpg'},
-    { title: 'limbe Botanical Garden', Image: './bg3.jpg'},
-    { title: 'The Ekom Nkam Waterfalls', Image: './bg4.jpg'},
-    { title: 'Down Beach', Image: './bg5.jpg'},
-    { title: 'Jully Kribi Residence', Image: './bg6.jpg'},
-    { title: 'The Mount Febe Hotel', Image: './bg3.jpg'},
-    { title: 'The Douala Rabingha Hotel', Image: './bg4.jpg'},
-    { title: 'The Adrien Beach Hotel', Image: './bg5.jpg'},
-    { title: 'The Residence Amar', Image: './bg6.jpg'},
+    { title: 'The Waza Park', Image: './bg1.jpg', type:"trip"},
+    { title: 'The Foumban Museum', Image: './bg2.jpg', type:"trip"},
+    { title: 'limbe Botanical Garden', Image: './bg3.jpg', type:"trip"},
+    { title: 'The Ekom Nkam Waterfalls', Image: './bg4.jpg', type:"trip"},
+    { title: 'Down Beach', Image: './bg5.jpg', type:"trip"},
+    { title: 'Jully Kribi Residence', Image: './bg6.jpg', type:"hotel"},
+    { title: 'The Mount Febe Hotel', Image: './bg3.jpg', type:"hotel"},
+    { title: 'The Douala Rabingha Hotel', Image: './bg4.jpg', type:"hotel"},
+    { title: 'The Adrien Beach Hotel', Image: './bg5.jpg', type:"hotel"},
+    { title: 'The Residence Amar', Image: './bg6.jpg', type:"hotel"}, 
 ]
 
 function Home() {
@@ -21,10 +21,13 @@ function Home() {
 
         <div className='card-container'>
             {cardData.map((card, index) => (
-                <div className='card' key={index}>
+                <div className={`card ${card.type}`} key={index}>
                     <img src={card.Image} alt={card.title} />
                     <h3>{card.title}</h3>
-                    <button>View</button>
+                    <link to={card.link}>
+                    <button className='view-btn'>View</button>
+                    </link>
+                    
                 </div>
             ))}
         </div>
